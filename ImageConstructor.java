50fd2bd7c19c3dc641e9a33611a5efdbec9847bc
Jirapat N <jean2541_sk@hotmail.com>
2017-03-10 20:38:58 +0700

    construc words with images

--- ImageConstructor.java
+++ ImageConstructor.java
@@ -10,6 +10,8 @@ public class ImageConstructor
 {
     public static java.util.HashMap<Character ,BufferedImage> alphabets = new HashMap<>();
     
+    private static final int IMAGE_SIZE = 50;
+    
     static {
         for(char c = 'a'; c <= 'z'; c++) {
             try {
@@ -19,18 +21,26 @@ public class ImageConstructor
                 System.out.println("Can't load alphabet " + c);
             }
         }
+        for(char c = '0'; c <= '9'; c++) {
+            try {
+                BufferedImage img = ImageIO.read(new File("images/" + c + ".jpg"));
+                alphabets.put((Character)c ,img);
+            } catch(IOException e) {
+                System.out.println("Can't load number " + c);
+            }
+        }
     }
     
     public static GreenfootImage create(String word) {
-        int len = word.length() * 100;
-        GreenfootImage img = new GreenfootImage(len, 100);
+        int len = word.length() * IMAGE_SIZE;
+        GreenfootImage img = new GreenfootImage(len, IMAGE_SIZE);
         BufferedImage buf = img.getAwtImage();
         Graphics graphic = buf.getGraphics();
         char[] chars = word.toCharArray();
         int offset = 0;
         for(char c : chars) {
             graphic.drawImage(alphabets.get(c), offset, 0, null);
-            offset += 100;
+            offset += IMAGE_SIZE;
         }
         return img;
     }
