dad8cf7cc14324057c7e147dd7c3a5c8874ea2f6
Jirapat N <jean2541_sk@hotmail.com>
2017-03-10 20:43:14 +0700

    count score

---
+++ ScoreDisplay.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ScoreDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ScoreDisplay extends Actor
+{
+    int value;
+    
+    public ScoreDisplay() {
+        this(0);
+    }
+    
+    public ScoreDisplay(int score){
+        setValue(score);
+    }
+
+    public void act() 
+    {
+        
+    }    
+    
+    public void add(int val) {
+        value += val;
+        updateImage();
+    }
+    
+    public void setValue(int val) {
+        value = val;
+        updateImage();
+    }
+    
+    public int getValue() {
+        return value;
+    }
+    
+    public void updateImage() {
+        setImage(ImageConstructor.create(String.format("%05d", value)));
+    }
+}
