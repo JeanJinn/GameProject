c75302758093d4766ebf5e06b6dac5f546bd1160
Jirapat N <jean2541_sk@hotmail.com>
2017-03-10 20:42:31 +0700

    game world

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
 
 /**
  * Write a description of class Game here.
@@ -8,14 +9,61 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Game extends World
 {
+    private SimpleTimer timer = new SimpleTimer();
+    private static List<String> allowedKeys = new ArrayList<>();
+    private Input input = new Input();
+    private ScoreDisplay scoreDisplay = new ScoreDisplay();
+    
+    static {
+        for(char c = 'a'; c <= 'z'; c++)
+            allowedKeys.add(c + "");
+    }
+    
+    int diff;
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
-    public Game()
+    public Game() {
+        this(0);
+    }
+    
+    public Game(int diff)
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1400, 876, 1);
+        this.diff = diff;
         addObject(new Junimo(), 700, 750);
+        addObject(input, 700, 50);
+        addObject(scoreDisplay, 940, 800);
     }
+    
+    public void act() {
+        if(timer.millisElapsed() >= (4000 - (1000 * diff))) {
+            addObject(new Word(), 200 + Greenfoot.getRandomNumber(1000), 0);
+            timer.mark();
+        }
+        String key = Greenfoot.getKey();
+        if(key != null && !key.isEmpty()) {
+            if(allowedKeys.contains(key)) {
+                input.setWord(input.getWord() + key);
+            } else if(key.equals("backspace") && !input.getWord().isEmpty()) {
+                input.setWord(input.getWord().substring(0, input.getWord().length()-1));
+            } else if(key.equals("enter")){
+                List<Word> words = this.getObjects(Word.class);
+                words.forEach((e) -> {
+                    if(e.getData().equals(input.getWord())) {
+                        this.removeObject(e);
+                        scoreDisplay.add(input.getWord().length() * (diff+1));
+                    }
+                });
+                input.setWord("");
+            }
+        }
+    }
+    
+    public int getDiff() {
+        return diff;
+    }
+    
 }
