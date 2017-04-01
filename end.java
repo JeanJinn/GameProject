63016fc09057a99f15c79cb58150d34a00fc8b87
nongtiny <tinyutb3612@gmail.com>
2017-04-02 04:07:12 +0700

    Add background effect in end world

--- end.java
+++ end.java
@@ -13,11 +13,34 @@ public class end extends World
      * Constructor for objects of class end.
      * 
      */
+    int frameTimer = 90;
     public end()
     {    
         super(1400, 876, 1);
     }
+    public void act(){
+        animation();
+    }
     public void stopped(){
+        
         Menu.bgm.pause();
+    }
+    
+     public void animation(){
+        if(frameTimer>0){
+            frameTimer--;
+        }
+       
+        if(frameTimer==60){
+             setBackground("junimobg1.png");
+        }
+        if(frameTimer==30){
+            setBackground("junimobg2.png");
+        }
+          if(frameTimer==0){
+            frameTimer=90;
+        }
+       
+       
     }
 }
