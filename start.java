2dac4c25ae7732d34a3b919ab38f94a52028e3b8
Jirapat N <jean2541_sk@hotmail.com>
2017-04-03 21:42:52 +0700

    input String(player name) when clicked start

--- start.java
+++ start.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import javax.swing.JOptionPane;
 /**
  * Write a description of class start here.
  * 
@@ -15,29 +15,30 @@ public class start extends Actor
     boolean mouseOver = false;
     public void act() 
     {
-        MouseInfo mouse = Greenfoot.getMouseInfo();  
+       MouseInfo mouse = Greenfoot.getMouseInfo();  
          
        if (!mouseOver && Greenfoot.mouseMoved(this))  
-        {  
-            setImage("start2.jpg");  
-            mouseOver = true;  
-         }  
-         if (mouseOver && Greenfoot.mouseMoved(null) && ! Greenfoot.mouseMoved(this))  
-         {  
-             setImage("startbutton.jpg");  
-             mouseOver = false;  
-         } 
+       {  
+           setImage("start2.jpg");  
+           mouseOver = true;  
+       }  
+       if (mouseOver && Greenfoot.mouseMoved(null) && ! Greenfoot.mouseMoved(this))  
+       {  
+           setImage("startbutton.jpg");  
+           mouseOver = false;  
+       } 
          
-        if(Greenfoot.mouseClicked(this)){
-            Greenfoot.playSound("buttonclick.mp3");
-            Menu world = (Menu)getWorld();
-            world.removeHelp();
-            world.removeReset();
-            for(int i=0; i<3 ; i++){
-                getWorld().addObject(new Lbutton(i), 700, (i+3)*150);
-            }
-            getWorld().removeObject(this);
-            Greenfoot.delay(20);
-        }
+       if(Greenfoot.mouseClicked(this)){
+           Greenfoot.playSound("buttonclick.mp3");
+           Menu world = (Menu)getWorld();
+           world.playerName = JOptionPane.showInputDialog("Player name: ");
+           world.removeHelp();
+           world.removeReset();
+           for(int i=0; i<3 ; i++){
+               getWorld().addObject(new Lbutton(i), 700, (i+3)*150);
+           }
+           getWorld().removeObject(this);
+           Greenfoot.delay(20);
+       }
     }    
 }
